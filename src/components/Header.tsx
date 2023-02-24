8bad1fb332b6a73eabe2b2f109f9f329d45a5ecf
Axel Jensen <jensen.axel@yahoo.com>
2023-02-24 10:48:52 -0700

    fixing header alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,32 +17,29 @@ const HeaderWrapper = (): JSX.Element => {
     return (
         <>
             <Header height={{ base: 60, md: 60 }} p='md'>
-                <Flex gap={10}>
-                    <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
-                        <Burger opened={opened} onClick={() => setOpened((o) => !o)} size='sm' mr='xl' />
-                    </MediaQuery>
-                    <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
-                        <span>
-                            <div>{count}</div>
-                            <div className={classes.hSpace} />
-                            <Anchor component={NavLink} to='/' className={classes.link} truncate>
-                                Home
-                            </Anchor>
-                            <Anchor component={NavLink} to='/count' className={classes.link} truncate>
-                                Count
-                            </Anchor>
-                            <Anchor component={NavLink} to='/tanstack' className={classes.link} truncate>
-                                Tanstack
-                            </Anchor>
-                            <Anchor component={NavLink} to='/table-demo' className={classes.link} truncate>
-                                React Table Demo
-                            </Anchor>
-                            <Anchor component={NavLink} to='/questions' className={classes.link} truncate>
-                                Questions (Basic Table)
-                            </Anchor>
-                        </span>
-                    </MediaQuery>
-                </Flex>
+                <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
+                    <Burger opened={opened} onClick={() => setOpened((o) => !o)} size='sm' mr='xl' />
+                </MediaQuery>
+                <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
+                    <Flex gap={10}>
+                        <div className={classes.hSpace} />
+                        <Anchor component={NavLink} to='/' className={classes.link} truncate>
+                            Home
+                        </Anchor>
+                        <Anchor component={NavLink} to='/count' className={classes.link} truncate>
+                            Count
+                        </Anchor>
+                        <Anchor component={NavLink} to='/tanstack' className={classes.link} truncate>
+                            Tanstack
+                        </Anchor>
+                        <Anchor component={NavLink} to='/table-demo' className={classes.link} truncate>
+                            React Table Demo
+                        </Anchor>
+                        <Anchor component={NavLink} to='/questions' className={classes.link} truncate>
+                            Questions (Basic Table)
+                        </Anchor>
+                    </Flex>
+                </MediaQuery>
             </Header>
         </>
     );
